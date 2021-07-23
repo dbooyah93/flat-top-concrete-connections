8872f824eb0d4b935afce9af429ad98c299a3061
dbooyah93 <davidmcruz1125@gmail.com>
2021-07-22 22:03:59 -0500

    Initial commit with images

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor( props ) {
+        super( props );
+        this.state = {
+        }
+    }
+    render() {
+    }
+  }
+  export default App;
