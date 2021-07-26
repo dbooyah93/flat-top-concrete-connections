6eef5d92c4c9c1bcde84279132b07a4bbeb521a8
David Boyea <dboyea@cimlbr.com>
2021-07-26 09:33:27 -0500

    Deploying a meaningful message rendered from a react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,9 +7,13 @@ class App extends React.Component {
         }
     }
     render() {
-        <div>
-            <h1>This is a JavaScript application rendering an element</h1>
-        </div>
+        return ( 
+            <div>
+                <h1>This is a JavaScript application rendering an element</h1>
+                <p>What i Have learned is that I need to develop locally for speed, but the website does deploy in about a minute after pushing</p>
+            </div>
+
+        )
     }
   }
   export default App;
