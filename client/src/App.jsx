dd453f2f31b7d8dd6d532631c122304ce2c17132
David Boyea <dboyea@cimlbr.com>
2021-07-26 16:56:40 -0500

    Raw bootstrap implemented, research shows React-boostrap library should be implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,8 +11,22 @@ class App extends React.Component {
             <div>
                 <h1>This is a JavaScript application rendering an element</h1>
                 <p>What i Have learned is that I need to develop locally for speed, but the website does deploy in about a minute after pushing</p>
+                <div class="row">
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="./resources/images/AndysConcrete/sideWalkDemoStart.jpg" alt="First slide"/>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="./resources/images/AndysConcrete/sideWalkDemoFirstMiddle.jpg" alt="Second slide"/>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="./resources/images/AndysConcrete/sideWalkDemoSecondMiddle.jpg" alt="Third slide"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         )
     }
   }
