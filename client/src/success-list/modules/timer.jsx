b7ef0fb9c434b6c4db4dc53a356f5dfdc475c88a
dbooyah93 <davidmcruz1125@gmail.com>
2023-09-13 23:19:32 -0500

    Initial attempt prior to researching

--- client/src/success-list/modules/timer.jsx
+++ client/src/success-list/modules/timer.jsx
@@ -2,7 +2,41 @@ import React from 'react';
 
 const Timer = ({}) => {
     let date = new Date();
-    let MMDDYYY= date.toLocaleDateString();
+    let MMDDYYYY = date.toLocaleDateString();
+    // save day
+    let HHMMSS = date.toLocaleTimeString(); 
+    // save time
+    let updateTimeDay = function () {
+        let verb = 1;
+        let newDate = new Date();
+        let newMMDDYYYY = newDate.toLocaleDateString();
+        let newHHMMSS = newDate.toLocaleTimeString();
+        MMDDYYYY = newMMDDYYYY;
+        HHMMSS = newHHMMSS;
+        console.log('Verb =' + verb);
+        verb++
+        launchTimmer();
+    }
+
+    let launchTimmer = function () {
+        setTimeout(()=> {
+            updateTimeDay();
+        }, 1000);
+    }
+
+    updateTimeDay();
+
+    // function { 
+    //   every 1000ms update the time variable
+    // }
+    return (
+        <div className="timer-root">
+            <p className="day">{ MMDDYYYY }</p>
+            <p className="timer">{ HHMMSS }</p>
+        </div>
+    )
+    // render
+    // the timer variable
 
 }
 
