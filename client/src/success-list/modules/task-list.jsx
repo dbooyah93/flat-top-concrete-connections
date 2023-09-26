e5b2c02373eb3dce1583c0e8a8cb199ab58bfc24
dbooyah93 <davidmcruz1125@gmail.com>
2023-09-26 09:34:25 -0500

    Pseudocode for task-list

--- client/src/success-list/modules/task-list.jsx
+++ client/src/success-list/modules/task-list.jsx
@@ -1,4 +1,9 @@
 // read local storage
+    // this should probably be a different module
+    // this module should accept properties
+    // one property should be the values from the localStorage
 // populate based on that
 // organize into year, month, day 
-// every item has a time stamp
+    // this module should control this layout
+    // read the time and day of the properties comming in
+// every item has a timestamp
